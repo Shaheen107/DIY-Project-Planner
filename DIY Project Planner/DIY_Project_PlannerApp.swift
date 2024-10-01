1874fb816d87c98ab5ac8f754617e304c58ade03
dsv <zxcv>
2024-10-02 00:15:44 +0500

    Initial Commit

---
+++ DIY Project Planner/DIY_Project_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIY_Project_PlannerApp.swift
+//  DIY Project Planner
+//
+//  Created by Muhammad Rizwan on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DIY_Project_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
